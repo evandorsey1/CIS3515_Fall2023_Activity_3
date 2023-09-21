6d3e61cd316342fb549eb07ec167b102ed2d4f13
Evan Dorsey <dorseyevan@gmail.com>
2023-09-21 15:11:48 -0400

    Finished TextSizeAdapter class

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import org.w3c.dom.Text
 
 
 // BaseAdapter is a class
@@ -21,15 +22,53 @@ class TextSizeAdapter(private val context: Context, private val textSizes: Array
     override fun getItemId(position: Int) = position.toLong()
 
 
-    override fun getView(position: Int, convertView: View?, p2: ViewGroup?): View {
-        val textView = TextView(context)
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        // // val textView = TextView(context)
+//        val textView : TextView
+//
+//
+//        // If we were given a view to recycle...
+//        if (convertView != null){
+//            textView = convertView as TextView
+//        }else {                                      // Else = we are not given a view to recycle
+//            textView = TextView(context)
+//            textView.setPadding(5, 10, 0, 10)
+//        }
+//
+//        textView.text = textSizes[position].toString()
+//        textView.textSize = textSizes[position].toFloat()
+//
+//        return textView
+        return (getDropDownView(position, convertView, parent) as TextView).apply{
+            textSize = 22f
+        }
+
+
+    }
+
+
+
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val textView : TextView
+
+
+        // If we were given a view to recycle...
+        if (convertView != null){
+            textView = convertView as TextView
+        }else {                                      // Else = we are not given a view to recycle
+            textView = TextView(context)
+            textView.setPadding(5, 10, 0, 10)
+        }
 
         textView.text = textSizes[position].toString()
         textView.textSize = textSizes[position].toFloat()
-        textView.setPadding(5, 10, 0, 10)
 
         return textView
     }
+
+
 
 
 }
